8e7cbbc4fa54f55917f8a8055c6061dd102c4507
Joserra2304 <joserra2304@gmail.com>
2023-05-10 20:58:50 +0200

    Conexión con la base de datos MySQL y paquete domain (Clint y Product) añadido

---
+++ src/main/java/com/svalero/telovendo/domain/Client.java
@@ -0,0 +1,49 @@
+package com.svalero.telovendo.domain;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    @NotBlank
+    private String name;
+
+    @Column
+    @NotBlank
+    private String surname;
+
+    @Column
+    @NotBlank
+    private String username;
+
+    @Column
+    @NotBlank
+    private int age;
+    @Column
+    @NotBlank
+    private String dni;
+
+    @Column
+    private LocalDate registerDate;
+    @Column
+    private boolean vip;
+
+    @OneToMany(mappedBy = "client")
+    @JsonBackReference(value = "client-product")
+    private List<Product> products;
+}
