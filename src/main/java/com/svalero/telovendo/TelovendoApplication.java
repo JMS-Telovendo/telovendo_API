2e815252bc55f84265cada604731b01190000f41
Joserra2304 <cydogamer@gmail.com>
2023-05-10 19:39:14 +0200

    Inicio del Proyecto

---
+++ src/main/java/com/svalero/telovendo/TelovendoApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.telovendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelovendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelovendoApplication.class, args);
+	}
+
+}
