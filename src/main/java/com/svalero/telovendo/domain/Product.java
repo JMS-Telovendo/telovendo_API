0e1413b4c0d10806bf3a9bb77413a761ab52c9bb
Joserra2304 <joserra2304@gmail.com>
2023-05-12 20:08:13 +0200

    modificaciones 2

--- src/main/java/com/svalero/telovendo/domain/Product.java
+++ src/main/java/com/svalero/telovendo/domain/Product.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "product")
+@Entity(name = "products")
 public class Product {
 
     @Id
--- src/main/java/com/svalero/telovendo/domain/User.java
+++ src/main/java/com/svalero/telovendo/domain/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "client")
+@Entity(name = "users")
 public class User {
 
     @Id
